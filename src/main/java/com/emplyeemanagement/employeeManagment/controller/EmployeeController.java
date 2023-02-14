d812370c57291ccc3a109981fa35f8ca3a75a07d
Pawan Kumar <mishrapawan671@gmail.com>
2023-02-15 00:59:48 +0530

    Added Controller to save Employee

--- src/main/java/com/emplyeemanagement/employeeManagment/controller/EmployeeController.java
+++ src/main/java/com/emplyeemanagement/employeeManagment/controller/EmployeeController.java
@@ -2,13 +2,15 @@ package com.emplyeemanagement.employeeManagment.controller;
 
 import com.emplyeemanagement.employeeManagment.Model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import com.emplyeemanagement.employeeManagment.repos.EmployeeRepository;
 
 import java.util.List;
-
+@CrossOrigin("http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -21,6 +23,20 @@ public class EmployeeController {
     public List<Employee> getAllEmployee()
     {
         return employeeRepository.findAll();
+    }
+
+    @PostMapping(value = "/AddEmployee",consumes = "application/json")
+        public ResponseEntity saveEmployee( @RequestBody Employee employee)
+    {
+        if(employeeRepository.save(employee)!=null)
+        {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).header("Message","Employee Saved").build();
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).build();
+        }
+
     }
 
 }
